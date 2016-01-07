0c3eb0d8a04b28b114ae76b10f83ff12cc186143
yenminh <ngocminh29011991@gmail.com>
2016-01-08 01:42:06 +0700

    fix media recorder

--- app/src/main/java/empire/stark/myapplication/RecorderCallService.java
+++ app/src/main/java/empire/stark/myapplication/RecorderCallService.java
@@ -8,6 +8,8 @@ import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -16,10 +18,12 @@ import java.io.IOException;
  * Code and Life
  */
 public class RecorderCallService extends Service {
+    private final String TAG = "[RecorderCallService]";
     private MediaRecorder mMediaRecorder;
     private boolean mIsRecording = false;
     private final String FOLDER_NAME = "RecorderPhoneCall";
     private String mFileType = ".3gp";
+    private TelephonyManager mTelephonyManager;
 
 
     @Nullable
@@ -31,32 +35,38 @@ public class RecorderCallService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //
-        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        PhoneStateListener phoneStateListener = new PhoneStateListener();
-        telephonyManager.listen(phoneStateListener,PhoneStateListener.LISTEN_CALL_STATE);
+        if (mTelephonyManager == null) {
+            Log.d(TAG, "start listen phone state");
+            mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            PhoneStateListener phoneStateListener = new PhoneStateListener();
+            mTelephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+        }
+
         //
         return START_STICKY;
     }
-    private void startRecording(String phoneNumber){
-        if(!mIsRecording){
+
+    private void startRecording(String phoneNumber) {
+        Log.d(TAG, " mIsRecording : " + mIsRecording);
+        if (!mIsRecording) {
 
             //
             String outPutFile = getOutputFile(phoneNumber);
-            if(outPutFile != null){
+            if (outPutFile != null) {
 
-                setupMediaRecorder(MediaRecorder.AudioSource.VOICE_CALL, MediaRecorder.OutputFormat.THREE_GPP, MediaRecorder.AudioEncoder.AAC);
+                setupMediaRecorder(MediaRecorder.AudioSource.VOICE_CALL, MediaRecorder.OutputFormat.THREE_GPP, MediaRecorder.AudioEncoder.AMR_NB);
                 //setupBitRate();
                 setOutPutFile(outPutFile);
-                try{
-                    mMediaRecorder.release();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
                 try {
-                    mMediaRecorder.start();
-                }catch (Exception e){
+
+                    mMediaRecorder.prepare();
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
+                Log.d(TAG, "mMediaRecorder is trying start");
+
+                mMediaRecorder.start();
+                Log.d(TAG, "mMediaRecorder start success");
 
                 mIsRecording = true;
             }
@@ -66,23 +76,26 @@ public class RecorderCallService extends Service {
     /**
      *
      */
-    private void setupMediaRecorder(int audioSource, int outputFormat, int audioEncoder){
+    private void setupMediaRecorder(int audioSource, int outputFormat, int audioEncoder) {
         mMediaRecorder = new MediaRecorder();
         mMediaRecorder.setAudioSource(audioSource);
         mMediaRecorder.setOutputFormat(outputFormat);
         mMediaRecorder.setAudioEncoder(audioEncoder);
     }
-    private void setupBitRate(int encodingBitrate, int samplingBitrate){
+
+    private void setupBitRate(int encodingBitrate, int samplingBitrate) {
         mMediaRecorder.setAudioEncodingBitRate(encodingBitrate);
         mMediaRecorder.setAudioSamplingRate(samplingBitrate);
     }
-    private void setOutPutFile(String path){
+
+    private void setOutPutFile(String path) {
         mMediaRecorder.setOutputFile(path);
     }
-    private String getOutputFile(String phoneNumber){
+
+    private String getOutputFile(String phoneNumber) {
         long time = System.currentTimeMillis();
-        String nameFile = phoneNumber+"_"+time;
-        File file = new File(Environment.getExternalStorageDirectory(), "/"+ FOLDER_NAME);
+        String nameFile = phoneNumber + "_" + time;
+        File file = new File(Environment.getExternalStorageDirectory(), "/" + FOLDER_NAME);
         if (!file.exists()) {
             file.mkdirs();
         }
@@ -92,16 +105,19 @@ public class RecorderCallService extends Service {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(recorderFile != null){
+        if (recorderFile != null) {
             return recorderFile.getAbsolutePath();
         }
         return null;
     }
-    private void stopRecording(){
+
+    private void stopRecording() {
         //
-        if(mIsRecording){
-            if(mMediaRecorder != null){
+        if (mIsRecording) {
+            if (mMediaRecorder != null) {
+                Log.d(TAG, "mMediaRecorder.stop()");
                 mMediaRecorder.stop();
+                mMediaRecorder.reset();
                 mMediaRecorder.release();
                 mIsRecording = false;
             }
@@ -111,24 +127,34 @@ public class RecorderCallService extends Service {
     }
 
     //
-    private class PhoneStateListener extends android.telephony.PhoneStateListener{
+    private class PhoneStateListener extends android.telephony.PhoneStateListener {
         @Override
         public void onCallStateChanged(int state, String incomingNumber) {
             super.onCallStateChanged(state, incomingNumber);
-            switch (state) {
-                case TelephonyManager.CALL_STATE_IDLE:
-                    stopRecording();
+            if (incomingNumber != null && !incomingNumber.equals("")) {
+                switch (state) {
+                    case TelephonyManager.CALL_STATE_IDLE:
+                        Log.d(TAG, " phone state : CALL_STATE_IDLE");
+                        stopRecording();
 
-                    break;
-                case TelephonyManager.CALL_STATE_RINGING:
+                        break;
+                    case TelephonyManager.CALL_STATE_RINGING:
 
-                    break;
-                case TelephonyManager.CALL_STATE_OFFHOOK:
+                        break;
+                    case TelephonyManager.CALL_STATE_OFFHOOK:
+                        Log.d(TAG, " phone state : CALL_STATE_OFFHOOK");
 
-                    startRecording(incomingNumber);
-                    break;
+                        startRecording(incomingNumber);
+                        break;
+                }
             }
+
         }
     }
 
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, " onDestroy");
+        super.onDestroy();
+    }
 }
